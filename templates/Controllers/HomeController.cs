53eb6e51cc930b9058525d5ac426d77141e1c0d1
Lily Womack <LilyBell@users.noreply.github.com>
2017-10-13 19:08:19 -0400

    Create HomeController.cs

---
+++ templates/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
